7ca507ab2ce163b29644e99c683f73c6f5384af1
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2024-01-19 15:08:06 +0200

    Add files via upload

--- keyboard/inline.py
+++ keyboard/inline.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# Все кнопки
+# Все кнопки для удаления
 button1 = InlineKeyboardButton(text="3 Oaks Gaming", callback_data="btn1")
 button2 = InlineKeyboardButton(text="Удалить", callback_data="del1")
 button3 = InlineKeyboardButton(text="PragmaticPlay", callback_data="btn2")
@@ -33,7 +33,7 @@ button31 = InlineKeyboardButton(text="Quickspin", callback_data="btn15")
 button32 = InlineKeyboardButton(text="Удалить", callback_data="del15")
 button33 = InlineKeyboardButton(text="Habanero", callback_data="btn16")
 button34 = InlineKeyboardButton(text="Удалить", callback_data="del16")
-button35 = InlineKeyboardButton(text="Востановить кнопки", callback_data="del17")
+button35 = InlineKeyboardButton(text="Востановить кнопки", callback_data="btn17")
 
 slots = InlineKeyboardMarkup(inline_keyboard=[
                                             [button1, button2],[button3, button4], 
@@ -47,3 +47,49 @@ slots = InlineKeyboardMarkup(inline_keyboard=[
                                             [button35],
                                             ],row_width=2)
 
+# Все кнопки для удаления
+button1 = InlineKeyboardButton(text="3 Oaks Gaming", callback_data="btn1")
+button2 = InlineKeyboardButton(text="Удалить", callback_data="del1")
+button3 = InlineKeyboardButton(text="PragmaticPlay", callback_data="btn2")
+button4 = InlineKeyboardButton(text="Удалить", callback_data="del2")
+button5 = InlineKeyboardButton(text="BetSoft", callback_data="btn3")
+button6 = InlineKeyboardButton(text="Удалить", callback_data="del3")
+button7 = InlineKeyboardButton(text="Red Tiger", callback_data="btn4")
+button8 = InlineKeyboardButton(text="Удалить", callback_data="del4")
+button9 = InlineKeyboardButton(text="Big Time Gaming", callback_data="btn5")
+button10 = InlineKeyboardButton(text="Удалить", callback_data="del5")
+button11 = InlineKeyboardButton(text="Relax Gaming", callback_data="btn6")
+button12 = InlineKeyboardButton(text="Удалить", callback_data="del6")
+button13 = InlineKeyboardButton(text="Play'n GO", callback_data="btn7")
+button14 = InlineKeyboardButton(text="Удалить", callback_data="del7")
+button15 = InlineKeyboardButton(text="Playson", callback_data="btn8")
+button16 = InlineKeyboardButton(text="Удалить", callback_data="del8")
+button17 = InlineKeyboardButton(text="Wazdan", callback_data="btn9")
+button20 = InlineKeyboardButton(text="Удалить", callback_data="del9")
+button21 = InlineKeyboardButton(text="Amatic", callback_data="btn10")
+button22 = InlineKeyboardButton(text="Удалить", callback_data="del10")
+button23 = InlineKeyboardButton(text="Yggdrasil", callback_data="btn11")
+button24 = InlineKeyboardButton(text="Удалить", callback_data="del11")
+button25 = InlineKeyboardButton(text="NolimitCity", callback_data="btn12")
+button26 = InlineKeyboardButton(text="Удалить", callback_data="del12")
+button27 = InlineKeyboardButton(text="BGaming", callback_data="btn13")
+button28 = InlineKeyboardButton(text="Удалить", callback_data="del13")
+button29 = InlineKeyboardButton(text="Endorphina", callback_data="btn14")
+button30 = InlineKeyboardButton(text="Удалить", callback_data="del14")
+button31 = InlineKeyboardButton(text="Quickspin", callback_data="btn15")
+button32 = InlineKeyboardButton(text="Удалить", callback_data="del15")
+button33 = InlineKeyboardButton(text="Habanero", callback_data="btn16")
+button34 = InlineKeyboardButton(text="Удалить", callback_data="del16")
+button35 = InlineKeyboardButton(text="Востановить кнопки", callback_data="btn17")
+
+slots_1 = InlineKeyboardMarkup(inline_keyboard=[
+                                            [button1, button2],[button3, button4], 
+                                            [button5, button6],[button7, button8],
+                                            [button9, button10],[button11, button12],
+                                            [button13, button14],[button15, button16],
+                                            [button17, button20],[button21, button22],
+                                            [button23, button24],[button25, button26],
+                                            [button27, button28],[button29, button30],
+                                            [button31, button32],[button33, button34],
+                                            [button35],
+                                            ],row_width=2)
